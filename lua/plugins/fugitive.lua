487c242365c0fbce6b84e6701b87bf842cf72afc
Andre da Luz <email.andredaluz@gmail.com>
2025-03-14 02:39:50 -0300

    Adding git keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,28 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        -- Declares git status function keymap
+        vim.keymap.set("n", "<leader>gs",  function()
+            vim.cmd('Git status')
+        end)
+
+        -- Declares git stash function keymap
+        vim.keymap.set("n", "<leader>gt", function()
+            vim.cmd('Git stash')
+        end)
+
+        -- Declares git stash apply function keymap
+        vim.keymap.set("n", "<leader>ga", function()
+            vim.cmd('Git stash apply')
+        end)
+
+        -- Toggles git blame window
+        vim.keymap.set("n", "<leader>gb", function()
+            if vim.bo.filetype == 'fugitiveblame' then
+                vim.cmd('close')
+            else
+                vim.cmd('Git blame')
+            end
+        end)
     end
 }
