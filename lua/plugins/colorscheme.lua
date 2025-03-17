e6c951c60a0a98a6ae890e121b109195b7fe34c2
André da Luz <email.andredaluz@gmail.com>
2025-03-17 12:03:03 -0300

    changed colorscheme to onedark warm

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,13 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour="mocha",
-			transparent_background = true,
-			show_end_of_buffer = true,
-		})
-
-		vim.cmd.colorscheme("catppuccin")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('onedark').setup {
+            style = 'warm',
+            transparent = true,
+            ending_tildes = true,
+        }
+        require('onedark').load()
+    end,
 }
