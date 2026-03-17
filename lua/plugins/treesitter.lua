3b986eaa87f8c047071f7f5de8a445444201d5bc
Andre <email.andredaluz@gmail.com>
2026-03-17 01:16:51 -0300

    new nvim-treesitter: ive fought it and ive won

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,58 @@
--- Treesitter for syntax highlighting
+-- Language syntax highlighting
 
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.config")
-
-        configs.setup({
+    config = function()
+        require("nvim-treesitter.config").setup({
             install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter",
-            ensure_installed = {
-                -- languages
-                "java",
-                "python",
-                "lua",
-                "javascript",
-                "typescript",
-                "tsx",
-                "php",
-                "vim",
-
-                -- web files
-                "json",
-                "json5",
-                "html",
-                "css",
-                "htmldjango",
-                "blade",
-
-                -- data related
-                "sql",
-                "query",
-                "csv",
-
-                -- some... files...
-                "dockerfile",
-                "latex",
-                "markdown",
-                "markdown_inline",
-                "yaml",
-
-                -- docs
-                "javadoc",
-                "jsdoc",
-                "luadoc",
-                "vimdoc",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-
-                    -- Cant find a way to make this work
-                    init_selection = "<C-s>",
-                    node_incremental = "<C-n>",
-                    scope_incremental = "<C-s>",
-                    node_decremental = "<C-m>",
-                }
-            }
         })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(ev)
+                pcall(vim.treesitter.start, ev.buf)
+            end,
+        })
+
+        local parsers = {
+            -- languages
+            "java",
+            "python",
+            "lua",
+            "javascript",
+            "typescript",
+            "php",
+
+            -- frameworks
+            "tsx",
+            "htmldjango",
+            "blade",
+
+            -- files
+            "json",
+            "html",
+            "css",
+            "csv",
+            "yaml",
+            "markdown",
+            "markdown_inline",
+
+            -- utils
+            "vim",
+            "sql",
+            "query",
+            "dockerfile",
+
+            -- docs
+            "javadoc",
+            "jsdoc",
+            "luadoc",
+            "vimdoc",
+        }
+
+        local install = require("nvim-treesitter.install")
+        for _, lang in ipairs(parsers) do
+            install.install(lang)
+        end
     end
 }
-
